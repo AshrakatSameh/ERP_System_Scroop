eb53579d0ae15fe4911b6422407351a1263e05b9
haidy-ismail <bata1nona@gmail.com>
2024-09-30 07:38:10 +0200

    ash-test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'forgetPassword', component: ForgetPasswordComponent},
+  {path:'registration', component:RegisterComponent},
+  {path:'sidebar', component:SidebarComponent},
+  {path:'login', component: LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    ForgetPasswordComponent,
+    SidebarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
