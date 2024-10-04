ab23baa9b849685474da447a60afdf8d1e8eceef
AshrakatSameh <ashrakatsameh3@gmail.com>
2024-10-05 01:17:18 +0300

    edits

--- src/app/components/sales/sales.component.ts
+++ src/app/components/sales/sales.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./sales.component.css']
 })
 export class SalesComponent {
+  buttons=['المعلومات الأساسية','المواقع و الفروع','المرفقات','المهام' ,'الحساب البنكي','الأشعارات والتذكير','التقارير','معلومات التواصل','بيانات الضريبه','الاستبيانات']
 
+  selectedButton: number | null = null; // To track which button is clicked
+
+  // Method to handle button click and show content
+  showContent(index: number): void {
+    this.selectedButton = index;
+  }
 }
